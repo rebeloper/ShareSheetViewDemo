fc87f8b484e6e94241d1093bb71abfbee0e36978
Alex Nagy <therebeloper@gmail.com>
2021-01-27 16:10:50 +0200

    Initial Commit

---
+++ ShareSheetViewDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShareSheetViewDemo
+//
+//  Created by Alex Nagy on 27.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShareSheetViewDemo/ShareSheetViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareSheetViewDemoApp.swift
+//  ShareSheetViewDemo
+//
+//  Created by Alex Nagy on 27.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct ShareSheetViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
