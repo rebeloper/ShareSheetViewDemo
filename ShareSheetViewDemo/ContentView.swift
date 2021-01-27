3662a9eb979072ada406012565dd26b24f317d52
Alex Nagy <therebeloper@gmail.com>
2021-01-27 16:41:58 +0200

    0.1.0

--- ShareSheetViewDemo/ContentView.swift
+++ ShareSheetViewDemo/ContentView.swift
@@ -6,11 +6,65 @@
 //
 
 import SwiftUI
+import ShareSheetView
 
 struct ContentView: View {
+    
+    @State private var isShareSheetViewPresented = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 12) {
+            
+            Button(action: {
+                isShareSheetViewPresented = true
+            }, label: {
+                Text("Share")
+            })
+            .sheet(isPresented: $isShareSheetViewPresented, content: {
+                ShareSheetView(activityItems: getActivityItems(), applicationActivities: getApplicationActivities(), excludedActivityTypes: getExcludedActivityTypes()) { (activityType, finished, activityItems, error) in
+                    if let error = error {
+                        print(error.localizedDescription)
+                        return
+                    }
+                    
+                    if let activityType = activityType {
+                        print(activityType)
+                    }
+                    
+                    print(finished)
+                    
+                    if let activityItems = activityItems {
+                        print(activityItems)
+                    }
+                }
+            })
+            
+            ShareSheetButton { () -> ShareSheetView in
+                ShareSheetView(activityItems: getActivityItems())
+            } label: { () -> AnyView in
+                AnyView(
+                    Image(systemName: "square.and.arrow.up")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 32, height: 32)
+                )
+            }
+            
+            Spacer()
+        }
+        .padding()
+    }
+    
+    func getActivityItems() -> [Any] {
+        return ["Hello ShareSheetView"]
+    }
+    
+    func getApplicationActivities() -> [UIActivity]? {
+        return nil
+    }
+    
+    func getExcludedActivityTypes() -> [UIActivity.ActivityType]? {
+        return [.postToFacebook]
     }
 }
 
